3078ceb4cf0841c158f21e8ecdaf69a76e50a19f
Shivam030902 <153266080+Shivam030902@users.noreply.github.com>
2024-02-18 22:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getFormvalue() {
     //Write your code here
 	let inps = document.querySelectorAll("input");
-	let firstName = inps[0];
-	let lastName = inps[1];
+	let firstName = inps[0].value;
+	let lastName = inps[1].value;
 	let res = firstName + " " + lastName
 	alert(res);
 }
